202efc57018e38686e13a809215cd46f7f72b0a5
l00580197 <longwei27@huawei.com>
2023-07-14 16:35:57 +0800

    add coredump monitor for sysboost

--- bin/coredump_monitor.rs
+++ bin/coredump_monitor.rs
@@ -2,26 +2,70 @@ use lazy_static::lazy_static;
 use std::collections::HashMap;
 use cnproc::PidMonitor;
 use cnproc::PidEvent;
+use std::sync::Mutex;
+use std::fs;
+use procfs::Process;
 
 lazy_static! {
-    static ref pid_maps: HashMap<u32, &'static str> = HashMap::new();
+    static ref MERGE_FILES: Mutex<Vec<String>> = Mutex::new(Vec::new());
+    static ref PID_INFOS: Mutex<HashMap<i32, String>> = Mutex::new(HashMap::new());
+}
+
+// add merged file into vector, provide for testcase
+fn add_merge_file(file_path: String) {
+    MERGE_FILES.lock().unwrap().push(file_path);
+}
+
+fn init_merge_file_list() {
+    MERGE_FILES.lock().unwrap().push(String::from("/usr/bin/bash.rto"));
 }
 
 fn process_exec_event(pid: i32) {
-    // pid_maps.insert(pid, pname);
+    // get execute file_path
+    let process = match Process::new(pid) {
+        Ok(process) => process,
+        Err(e) => {
+            log::error!("Failed to get execte process: {}", e);
+            return;
+        }
+    };
+    let file_path = match process.exe() {
+        Ok(file_path) => file_path,
+        Err(e) => {
+            log::error!("Failed to get excute file path: {}", e);
+            return;
+        }
+    };
+    if MERGE_FILES.lock().unwrap().contains(&file_path.as_path().display().to_string()) == false {
+        return;
+    }
+    PID_INFOS.lock().unwrap().insert(pid, String::from(file_path.to_str().unwrap()));
 }
 
 fn process_coredump_event(pid: i32) {
-    println!("lyt coredump pid: {}", pid);
+    // get file path by pid
+    if PID_INFOS.lock().unwrap().contains_key(&pid) == false {
+        log::info!("{} is not exist in PID_INFOS!", pid);
+        return;
+    }
+    
+    if let Some(file_path) = PID_INFOS.lock().unwrap().get(&pid) {
+        log::info!("{} has create a coredump!", file_path);
+        if MERGE_FILES.lock().unwrap().contains(&file_path) {
+            fs::remove_file(&file_path).expect("File delete failed!");
+        }
+    }
+
+    PID_INFOS.lock().unwrap().remove(&pid);
 }
 
 fn process_exit_event(pid: i32) {
-    // pid_maps.remove(pid)
+    PID_INFOS.lock().unwrap().remove(&pid);
 }
 
-fn main_loop() {
+pub fn coredump_monitor_loop() {
+    init_merge_file_list();
     let mut monitor = PidMonitor::new().unwrap();
-
     loop {
         match monitor.recv() {
             None => {}
@@ -36,3 +80,4 @@ fn main_loop() {
         }
     }
 }
+
