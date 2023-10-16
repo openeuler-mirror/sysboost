d95e3da20fbdeadd31ca933a4e89d28b9e7b54a6
Liu Yuntao <liuyuntao10@huawei.com>
2023-10-16 20:27:33 +0800

    gather all scripts into "scripts" folder

--- scripts/devops-env.sh
+++ scripts/devops-env.sh
@@ -4,5 +4,8 @@ set -x
 # compat openEuler 22.03 LTS
 # update pkg >= kernel-5.10.0-60.99.0.123.oe2203
 
-# install tools
+# build dependencies
 sudo yum install -y gcc make meson cargo xz-devel kernel kernel-devel ncurses-devel
+
+# rpm-build dependencies
+sudo yum install -y rpm-build native-turbo-devel rust-packaging
